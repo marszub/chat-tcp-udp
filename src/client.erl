36592e8c34059778e99107c3e092f4ce6b0ce906
Marcin Szubert <marszub@gmail.com>
2022-03-02 22:15:29 +0100

    Communication over tcp

--- src/client.erl
+++ src/client.erl
@@ -2,11 +2,33 @@
 -author("marszub").
 
 %% API
--export([start/0]).
+-export([start/3, send/1]).
 
-start() ->
-  SomeHostInNet = "localhost", % to make it runnable on one machine
-  {ok, Sock} = gen_tcp:connect(SomeHostInNet, 5678,
-    [binary, {packet, 0}]),
-  ok = gen_tcp:send(Sock, "Some Data"),
-  ok = gen_tcp:close(Sock).
+start(Address, Port, Name) ->
+  {ok, Socket} = gen_tcp:connect(Address, Port, [binary, {packet, 0}]),
+  ok = gen_tcp:send(Socket, Name ++ [2]),
+  register(receiver, spawn(fun() -> receiver_process(Socket, []) end)),
+  register(sender, spawn(fun() -> sender_process(Socket) end)).
+
+receiver_process(Socket, Buffer) ->
+  case gen_tcp:recv(Socket, 0) of
+    {ok, 0} ->
+      io:format(lists:reverse(Buffer)),
+      io:format("~n"),
+      receiver_process(Socket, []);
+    {ok, Byte} ->
+      receiver_process(Socket, [Byte | Buffer]);
+    {error, closed} ->
+      ok
+  end.
+
+sender_process(Socket) ->
+  receive
+    Message ->
+      gen_tcp:send(Socket, Message ++ [0]),
+      sender_process(Socket)
+  end.
+
+send(Message) ->
+  sender ! Message.
+
--- src/server.erl
+++ src/server.erl
@@ -9,32 +9,45 @@ start(Port) ->
   {ok, ServerSocket} = gen_tcp:listen(Port, [binary, {packet, 0}, {active, false}]),
   ClientDict = dict:new(),
   sender = spawn(fun() -> sender_process(ClientDict) end),
-
   ok = listen(ServerSocket),
   ok = gen_tcp:close(ServerSocket).
 
 listen(ServerSocket)->
   {ok, ClientSocket} = gen_tcp:accept(ServerSocket),
-  Pid = spawn(?MODULE, receiver_process, [ClientSocket]),
+  spawn(fun() -> receiver_process(ClientSocket, []) end),
   listen(ServerSocket).
 
 sender_process(ClientDict) ->
   receive
     {add, ClientSocket, Nick} ->
-      sender_process();
+      sender_process(dict:store(ClientSocket, Nick, ClientDict));
     {remove, ClientSocket} ->
-      sender_process();
-    {send, Message} ->
-      sender_process()
+      sender_process(dict:erase(ClientSocket, ClientDict));
+    {send, Sender, Message} ->
+      send_broadcast(dict:to_list(ClientDict), Sender, Message),
+      sender_process(ClientDict);
+    close ->
+      ok
   end.
 
-receiver_process(ClientSocket)->
-  ok = gen_tcp:close(ClientSocket).
+send_broadcast([], _, _) ->
+  ok;
+send_broadcast([{Sender, _} | Tail], Sender, Message) ->
+  send_broadcast(Tail, Sender, Message);
+send_broadcast([{Socket, Name} | Tail], Sender, Message) ->
+  ok = gen_tcp:send(Socket, Name ++ ": " ++ Message),
+  send_broadcast(Tail, Sender, Message).
 
-do_recv(Sock, Bs) ->
-  case gen_tcp:recv(Sock, 0) of
-    {ok, B} ->
-      do_recv(Sock, [Bs, B]);
+receiver_process(ClientSocket, Buffer)->
+  case gen_tcp:recv(ClientSocket, 0) of
+    {ok, 0} ->
+      sender ! {send, ClientSocket, lists:reverse([0 | Buffer])},
+      receiver_process(ClientSocket, []);
+    {ok, 2} ->
+      sender ! {add, ClientSocket, lists:reverse(Buffer)},
+      receiver_process(ClientSocket, []);
+    {ok, Byte} ->
+      receiver_process(ClientSocket, [Byte | Buffer]);
     {error, closed} ->
-      {ok, list_to_binary(Bs)}
+      sender ! {remove, ClientSocket}
   end.
